2e46e633d31a434283766f89db4d2e9e417b5546
albertarielw <albertariel2002@gmail.com>
2022-12-01 02:53:11 +0800

    adds router for module prototype

---
+++ database/database.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func db() {
+	DATABASE_URL := "postgres://ddbrflhccgsbnv:9dea2c78cfd420c6af3f51e1bac56c9a15ed73dc73ad1b50172c5e973b613e37@ec2-44-206-137-96.compute-1.amazonaws.com:5432/devchlvlplpfpu"
+	db, err := sql.Open("postgres", os.Getenv(DATABASE_URL))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(db.Stats().InUse)
+}
