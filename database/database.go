29db58283b809c7f987980d0dbd4bcca3ae8a11b
Berted <46164341+Berted@users.noreply.github.com>
2022-11-30 21:48:10 +0800

    DB: Add skeleton DB connection

--- database/database.go
+++ database/database.go
@@ -2,15 +2,62 @@ package database
 
 import (
 	"database/sql"
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-func db() {
-	DATABASE_URL := "postgres://ddbrflhccgsbnv:9dea2c78cfd420c6af3f51e1bac56c9a15ed73dc73ad1b50172c5e973b613e37@ec2-44-206-137-96.compute-1.amazonaws.com:5432/devchlvlplpfpu"
-	db, err := sql.Open("postgres", os.Getenv(DATABASE_URL))
+// Loads Database URI from .env file
+func loadDbUri() string {
+	err := godotenv.Load("database/.env")
+
 	if err != nil {
 		panic(err)
 	}
+
+	log.Printf("Env value: %s", os.Getenv("DATABASE_URI"))
+
+	return os.Getenv("DATABASE_URI")
+}
+
+func GetDb() *sql.DB {
+	db, err := sql.Open("postgres", loadDbUri())
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+type Module struct {
+	Id   string
+	Name string
+	Desc string
+}
+
+func GetModules(db *sql.DB) []Module {
+	rows, err := db.Query("SELECT * FROM Modules")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var modules []Module
+
+	for rows.Next() {
+		var mod Module
+		err := rows.Scan(&mod.Id, &mod.Name, &mod.Desc)
+		if err != nil {
+			panic(err)
+		}
+		modules = append(modules, mod)
+	}
+
+	if rows.Err() != nil {
+		panic(err)
+	}
+
+	return modules
 }
